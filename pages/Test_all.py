00c3d5b1c33e3919d6cdb4a9bca939fbb7d0915a
Ejic01 <vladimir_kuznecov_98@mail.ru>
2024-01-16 20:10:15 +0300

    Revert "Lesson 4.2.8 passe"
    This reverts commit 0475b17583c47cb3e59b74f93ca980b7afec4786.

--- pages/Test_all.py
+++
@@ -1,5 +0,0 @@
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
